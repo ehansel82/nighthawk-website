e7731d51903c7b8857f58b77557a15f7a303c976
Eric Hansel <e.hansel.01@gmail.com>
2016-08-22 21:57:00 -0400

    several changes to navbar and some styling

--- app/app.js
+++ app/app.js
@@ -1,10 +1,41 @@
 var app = angular.module("nighthawkApp", ["ngRoute"]);
+
 app.config(function($routeProvider) {
     $routeProvider
     .when("/", {
-        templateUrl : "app/main.htm"
+        templateUrl : "app/home.htm"
     })
-    .when("/red", {
-        templateUrl : "app/red.htm"
+    .when("/schedule", {
+        templateUrl : "app/schedule.htm"
+    })
+    .when("/about", {
+        templateUrl : "app/about.htm"
     })
 });
+
+app.controller('navCtrl', ['$scope', function($scope) {
+    $scope.isAboutActive = false;
+    $scope.isScheduleActive = false;
+    $scope.isHomeActive = false;
+
+    $scope.setScheduleActive = function(){
+      $scope.clearAllActive();
+      $scope.isScheduleActive = true;
+    };
+
+    $scope.setAboutActive = function(){
+      $scope.clearAllActive();
+      $scope.isAboutActive = true;
+    };
+
+    $scope.setHomeActive = function(){
+      $scope.clearAllActive();
+      $scope.isHomeActive = true;
+    };
+
+    $scope.clearAllActive = function(){
+      $scope.isAboutActive = false;
+      $scope.isScheduleActive = false;
+      $scope.isHomeActive = false;
+    };
+}]);
--- app/navCtrl.js
+++
@@ -1,7 +0,0 @@
-var app = angular.module('nighthawkApp',[]);
-
-app.controller('navCtrl', ['$scope', '$location', function($scope, $location) {
-    $scope.isActive = function (viewLocation) { 
-        return viewLocation === $location.path();
-    };
-}]);
